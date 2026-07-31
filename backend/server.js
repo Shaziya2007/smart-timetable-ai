7778f831d8267746448448dc4e8e18532fac1aba
Shaziya Fathima S F <shaziyafakrudeen@gmail.com>
2026-07-31 12:10:27 +0530

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Smart Timetable Backend is Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
